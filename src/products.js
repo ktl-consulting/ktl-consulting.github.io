6cefc180e3c685450b2721981d1c5abcbfd79b23
Kiet Ly <kietly@pop-os.localdomain>
2021-05-16 14:19:33 -0400

    replace with AWS migration

--- src/products.js
+++ src/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     _id: '1',
     name: 'AI',
-    image: '/images/AI.png',
+    image: '/company/images/AI.png',
     description:'We can develop and integrate AI into new or existing apps. We can deploy the customize ML/DL models either on the cloud or on-premise. Contact us now.',
     category: 'AI',
     countInStock: 10,
@@ -10,15 +10,15 @@ const products = [
   },
   {
     _id: '2',
-    name: 'Cybersecurity Tools',
-    image: '/images/cyber.png',
-    description: 'Do you need to assess and secure your network? Contact us now.',
+    name: 'Cloud migration',
+    image: 'https://www.clipartmax.com/png/middle/195-1955494_aws-simple-icons-aws-cloud-aws-cloud.png',
+    description: 'Do you need help with AWS migration? Contact us now.',
     category: 'cyber',
   },
   {
     _id: '3',
     name: 'Front End Development',
-    image: '/images/ui.png',
+    image: '/company/images/ui.png',
     description: 'Do you need Mobile/Desktop solutions. We can design and build solutions using React, Flutter, or native development using Swift, Java. Contact us now.',
     category: 'UI',
   },
